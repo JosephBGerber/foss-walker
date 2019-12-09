89ef884a51721ba6772d6c901114509ba577b242
joseph <josephbgerber@gmail.com>
2019-12-09 17:38:53 -0500

    Add rtfm support

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,99 @@
+#![feature(alloc_error_handler)]
 #![no_std]
 #![no_main]
 
-// pick a panicking behavior
-// extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// extern crate panic_abort; // requires nightly
-// extern crate panic_itm; // logs messages over ITM; requires ITM support
-extern crate panic_semihosting;
+#![allow(dead_code, unused_variables, unused_imports)]
+
+extern crate alloc;
+use alloc_cortex_m::CortexMHeap;
+use alloc::boxed::Box;
+
+#[global_allocator]
+static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
+
+use panic_semihosting;
+use cortex_m_semihosting::{debug, hprintln};
 
 use cortex_m;
-use cortex_m_rt::entry;
-use cortex_m_semihosting::hprintln;
-
-use crate::hal::{prelude::*, stm32};
 use stm32f4xx_hal as hal;
+use embedded_hal;
+use rtfm::app;
 
-#[entry]
-fn main() -> ! {
-    let cortex_p = cortex_m::Peripherals::take().unwrap();
-    let p = stm32::Peripherals::take().unwrap();
+use crate::hal::{prelude::*, stm32, spi, gpio::ExtiPin};
 
-    unsafe {
-        p.FLASH.keyr.write(|w| w.bits(0x4567_0123));
-        p.FLASH.keyr.write(|w| w.bits(0xCDEF_89AB));
-    };
+use accelerometer::Accelerometer;
 
-    let gpioa = p.GPIOA.split();
-    let mut led = gpioa.pa0.into_push_pull_output();
+mod flash;
+mod lis3dsh;
 
-    let rcc = p.RCC.constrain();
-    let clocks = rcc.cfgr.sysclk(48.mhz()).freeze();
+use lis3dsh::{LIS3DSH, I16x3};
 
-    let mut delay = hal::delay::Delay::new(cortex_p.SYST, clocks);
-
-    loop {
-        led.set_high().unwrap();
-        hprintln!("ON").unwrap();
-        delay.delay_ms(1000_u32);
-        led.set_low().unwrap();
-        hprintln!("OFF").unwrap();
-        delay.delay_ms(1000_u32);
+#[app(device = stm32f4xx_hal::stm32, peripherals = true)]
+const APP: () = {
+    struct Resources {
+        #[init(0)]
+        interrupts: usize,
+        accelerometer: Box<dyn Accelerometer<I16x3, Error = lis3dsh::Error<spi::Error, !>>>,
     }
+
+    #[init]
+    fn init(cx: init::Context) -> init::LateResources {
+        let core: cortex_m::Peripherals = cx.core;
+
+        let device: stm32::Peripherals = cx.device;
+        
+        let start = cortex_m_rt::heap_start() as usize;
+        let size = 1024;
+        unsafe { ALLOCATOR.init(start, size) }
+
+        let mut exti1 = device.EXTI;
+
+        let gpioe = device.GPIOE.split();
+        let mut pe0 = gpioe.pe0.into_floating_input();
+        let cs = gpioe.pe3.into_push_pull_output();
+
+        pe0.trigger_on_edge(&mut exti1, hal::gpio::Edge::RISING);
+
+        let gpioa = device.GPIOA.split();
+        let sck = gpioa.pa5.into_alternate_af5();
+        let miso = gpioa.pa6.into_alternate_af5();
+        let mosi = gpioa.pa7.into_alternate_af5();
+        let pins = (sck, miso, mosi);
+
+        let rcc = device.RCC.constrain();
+        let clocks = rcc.cfgr.freeze();
+
+        let spi = hal::spi::Spi::spi1(device.SPI1, pins, embedded_hal::spi::MODE_3, 4_000_000.hz(), clocks);
+
+        let mut accelerometer= LIS3DSH::new(spi, cs).unwrap();
+        accelerometer.enable_dr_interrupt().unwrap();
+        let acceleration = accelerometer.acceleration().unwrap();
+        accelerometer.calibrate_acceleration(acceleration).unwrap();
+
+        let accelerometer = Box::from(accelerometer);
+
+        hprintln!("INIT").unwrap();
+
+        init::LateResources{accelerometer}
+    }
+
+    #[task(binds = EXTI1, resources = [interrupts])]
+    fn EXTI1(cx: EXTI1::Context) {
+        let interrupts: &mut usize = cx.resources.interrupts;
+
+        *interrupts += 1;
+
+        hprintln!(
+            "EXTI1 called {} time{}",
+            *interrupts,
+            if *interrupts > 1 { "s" } else { "" }
+        )
+        .unwrap();
+    }
+};
+
+
+#[alloc_error_handler]
+pub fn rust_oom(_: core::alloc::Layout) -> ! {
+    panic!("OUT OF MEMORY");
 }
